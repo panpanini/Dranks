388224ac9eb3464181d4316956b33cdd860b02dd
panpanini <panpanini@mercari.com>
2020-09-06 14:29:53 +0900

    Add Compose UI test

---
+++ app/src/androidTest/java/jp/co/panpanini/dranks/CocktailListTest.kt
@@ -0,0 +1,26 @@
+package jp.co.panpanini.dranks
+
+import androidx.ui.test.assertCountEquals
+import androidx.ui.test.createComposeRule
+import androidx.ui.test.onAllNodesWithText
+import jp.co.panpanini.dranks.cocktail.Cocktail
+import jp.co.panpanini.dranks.cocktail.ui.CocktailRow
+import org.junit.Rule
+import org.junit.Test
+
+class CocktailListTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun cocktail_row_should_display_title() {
+        val cocktail = Cocktail(1, "Neko", null, null, null, null, null, true, null, null, "http://placekitten.com/200/200", listOf(), true, null)
+
+        composeTestRule.setContent {
+            CocktailRow(cocktail)
+        }
+
+        onAllNodesWithText("Neko").assertCountEquals(1)
+    }
+}
