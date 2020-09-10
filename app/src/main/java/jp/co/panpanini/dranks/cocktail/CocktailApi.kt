dab460cbe83e87d2c405789f44fca01f6c8d3f18
panini <panpanini@mercari.com>
2020-09-11 02:32:01 +0900

    Wrap DrinkResponse in NetworkResponse

--- app/src/main/java/jp/co/panpanini/dranks/cocktail/CocktailApi.kt
+++ app/src/main/java/jp/co/panpanini/dranks/cocktail/CocktailApi.kt
@@ -1,9 +1,10 @@
 package jp.co.panpanini.dranks.cocktail
 
+import jp.co.panpanini.dranks.network.NetworkResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface CocktailApi {
     @GET("/api/json/v1/1/search.php")
-    suspend fun search(@Query("s") name: String): DrinkResponse
+    suspend fun search(@Query("s") name: String): NetworkResponse<DrinkResponse>
 }
