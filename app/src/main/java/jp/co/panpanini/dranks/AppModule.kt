a68aec6dc034b141c414fc046585fae67c3bc2d6
panini <panpanini@mercari.com>
2020-09-11 02:31:39 +0900

    Pass NetworkResponseAdapter to Retrofit

--- app/src/main/java/jp/co/panpanini/dranks/AppModule.kt
+++ app/src/main/java/jp/co/panpanini/dranks/AppModule.kt
@@ -4,6 +4,7 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import jp.co.panpanini.dranks.cocktail.CocktailApi
 import jp.co.panpanini.dranks.cocktail.flux.CocktailFluxProvider
+import jp.co.panpanini.dranks.network.NetworkResponseAdapterFactory
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -18,6 +19,7 @@ val appModule = module {
     single {
         Retrofit.Builder()
             .baseUrl("https://www.thecocktaildb.com")
+            .addCallAdapterFactory(NetworkResponseAdapterFactory())
             .addConverterFactory(MoshiConverterFactory.create(get()))
             .build()
     }
