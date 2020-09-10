1fbf4a46936e22fb66c7bfee1cf1a0eca4fe9213
panini <panpanini@mercari.com>
2020-09-11 02:38:21 +0900

    Move error handling to a separate function

--- app/src/main/java/jp/co/panpanini/dranks/cocktail/flux/CocktailActionCreator.kt
+++ app/src/main/java/jp/co/panpanini/dranks/cocktail/flux/CocktailActionCreator.kt
@@ -5,6 +5,7 @@ import jp.co.panpanini.dranks.cocktail.Drink
 import jp.co.panpanini.dranks.flux.ActionCreator
 import jp.co.panpanini.dranks.flux.Dispatcher
 import jp.co.panpanini.dranks.network.Failure
+import jp.co.panpanini.dranks.network.NetworkResponse
 import jp.co.panpanini.dranks.network.ServerError
 import jp.co.panpanini.dranks.network.Success
 import kotlinx.coroutines.*
@@ -23,8 +24,7 @@ class CocktailActionCreator(
         scope.launch(coroutineDispatcher) {
             val cocktails = when (val response = cocktailApi.search(cocktail)) {
                 is Success -> response.data.drinks?.map(Drink::toCocktail)
-                is ServerError -> return@launch dispatcher.dispatch(NoCocktailsFound)
-                is Failure -> return@launch dispatcher.dispatch(NoCocktailsFound)
+                else -> return@launch handleError(response)
             }
 
             dispatcher.dispatch(
@@ -37,6 +37,14 @@ class CocktailActionCreator(
         scope.launch(coroutineDispatcher) {
             delay(5000)
             dispatcher.dispatch(NoCocktailsFound)
+        }
+    }
+
+    private fun handleError(response: NetworkResponse<*>) {
+        when (response) {
+            is ServerError -> dispatcher.dispatch(NoCocktailsFound)
+            is Failure -> dispatcher.dispatch(NoCocktailsFound)
+            is Success -> return // do nothing
         }
     }
 }
