08236e3cddc09de56c40f4129bbda2b69493d597
panini <panpanini@mercari.com>
2020-09-13 01:34:22 +0900

    Update SignalProperty to use LiveEvent

--- app/src/main/java/jp/co/panpanini/dranks/flux/SignalProperty.kt
+++ app/src/main/java/jp/co/panpanini/dranks/flux/SignalProperty.kt
@@ -1,9 +1,6 @@
 package jp.co.panpanini.dranks.flux
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
 
 class SignalProperty<T> private constructor() {
 
@@ -12,37 +9,15 @@ class SignalProperty<T> private constructor() {
         fun <T> create() = SignalProperty<T>()
     }
 
-    private val _liveData = MutableLiveData<Event<T?>>()
-
-    private val mediator = MediatorLiveData<T>().apply {
-        addSource(Transformations.map(_liveData) {
-            it?.getContentIfNotHandled()
-        }) { it?.let(::setValue) }
-    }
+    private val _liveEvent = LiveEvent<T>()
 
     fun set(value: T?) {
         // only emit if someone is listening
-        if (_liveData.hasActiveObservers()) {
-            _liveData.value = Event(value)
+        if (_liveEvent.hasActiveObservers()) {
+            value?.let(_liveEvent::postValue)
         }
     }
 
     val liveData: LiveData<T>
-        get() = mediator
-
-    class Event<T>(private val value: T?) {
-        private var hasBeenHandled = false
-
-        /**
-         * Returns the content and prevents its use again.
-         */
-        fun getContentIfNotHandled(): T? {
-            return if (hasBeenHandled) {
-                null
-            } else {
-                hasBeenHandled = true
-                value
-            }
-        }
-    }
+        get() = _liveEvent
 }
