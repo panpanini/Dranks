7d62d93772e0cf8a5aab6ac220aac51f7639bb18
panini <panpanini@mercari.com>
2020-09-13 01:41:52 +0900

    Add default values to cocktail model

--- app/src/main/java/jp/co/panpanini/dranks/cocktail/Cocktail.kt
+++ app/src/main/java/jp/co/panpanini/dranks/cocktail/Cocktail.kt
@@ -4,18 +4,18 @@ package jp.co.panpanini.dranks.cocktail
 data class Cocktail(
     val id : Int,
     val name : String,
-    val alternativeName : String?,
-    val tags : String?,
-    val videoUrl : String?,
-    val category : String?,
-    val iba : String?,
-    val alcoholic : Boolean,
-    val glass : String?,
-    val instructions : String?,
-    val thumbUrl : String?,
-    val ingredients: List<Ingredient>,
-    val creativeCommons: Boolean,
-    val dateModified : String?
+    val alternativeName : String? = null,
+    val tags : String? = null,
+    val videoUrl : String? = null,
+    val category : String? = null,
+    val iba : String? = null,
+    val alcoholic : Boolean = true,
+    val glass : String? = null,
+    val instructions : String? = null,
+    val thumbUrl : String? = null,
+    val ingredients: List<Ingredient> = listOf(),
+    val creativeCommons: Boolean = false,
+    val dateModified : String? = null
 )
 
 data class Ingredient(
